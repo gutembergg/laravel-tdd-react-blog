5066d0eede798f1791ba5be08639e4aa410bdc61
gutembergg <gmascarenhas3001@gmail.com>
2023-10-03 12:01:13 +0200

    test view dashboard ok

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="p-8 md:min-w-[900px]">
-        <x-admin.store-post-form :categories="$categories" />
+       {{--  <x-admin.store-post-form :categories="$categories" /> --}}
         <x-admin.last-posts :posts="$posts" />
     </div>
 @endsection
--- tests/Feature/Admin/DashboardTest.php
+++ tests/Feature/Admin/DashboardTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Admin;
 
 use App\Enums\Role\RoleEnum;
 use App\Models\Author;
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Database\Seeders\CategoriesSeeder;
 use Database\Seeders\RoleSeeder;
@@ -29,7 +31,39 @@ class DashboardTest extends TestCase
             ->assignRole(fake()
                     ->randomElement([RoleEnum::SUPER_ADMIN->value, RoleEnum::EDITOR->value]));
 
-        $response = $this->actingAs($user)->get(route('dashboard'));
+        $authentinadedUser = $this->actingAs($user);
+        $categories = Category::all();
+
+        $author = Author::factory()
+            ->has(Post::factory(5)
+            ->hasAttached($categories->random(2)))
+            ->create([
+                'name' => $user->name,
+                'user_id' => $user->id,
+            ]);
+
+        $posts = $author['posts'];
+        
+        $response = $this->actingAs($user)->get(route('dashboard', [
+            'category' => $categories,
+            'posts' => $posts,
+        ]));
+
+        $lastPosts = $response['posts'];
+
+        $view = $this->blade(
+            '<x-admin.store-post-form :categories="$categories" />',
+            ['categories' => $categories]
+        );
+        $view->assertSee($categories->first()->name);
+
+        $view = $this->blade(
+            '<x-admin.last-posts :posts="$posts" />',
+            ['posts' => $lastPosts]
+        );
+        $view->assertSee($lastPosts->first()->title);
+
+       // $view = $this->view('admin.index', ['categories' => $categories, 'posts' => $lastPosts]);
 
         $response->assertStatus(200);
 
