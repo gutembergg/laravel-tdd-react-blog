b0ce39c098ee87077168d520492ea7adeb6dc7a1
gutembergg <gmascarenhas3001@gmail.com>
2023-10-22 23:30:42 +0200

    Feat: added test messages error and loading search component

--- blog-react/src/pages/SearchPost/SearchPost.test.tsx
+++ blog-react/src/pages/SearchPost/SearchPost.test.tsx
@@ -8,16 +8,18 @@ import * as useApiRequest from '../../hooks/useApiRequest';
 const useApiRequestSpy = vi.spyOn(useApiRequest, 'useApiRequests');
 
 const dataSpy = {
-    data: [
-        {
-            id: 1,
-            title: 'test-title',
-            content: 'test content',
-            medias: [],
-            author: { id: 1, name: 'test-author' },
-            created_at: '2023-10-12T21:24:18.000000Z',
-        },
-    ],
+    data: {
+        data: [
+            {
+                id: 1,
+                title: 'test-title',
+                content: 'test content',
+                medias: [],
+                author: { id: 1, name: 'test-author' },
+                created_at: '2023-10-12T21:24:18.000000Z',
+            },
+        ],
+    },
     error: false,
     fetchData: vi.fn(),
     isLoading: false,
@@ -32,6 +34,28 @@ describe('<SearchPost>', () => {
         useApiRequestSpy.mockReturnValue(dataSpy);
         const { getByText } = renderedComponent();
 
-        expect(getByText(dataSpy.data[0].title)).toBeInTheDocument();
+        expect(getByText(dataSpy.data.data[0].title)).toBeInTheDocument();
+    });
+
+    test('Should display error if api response error', () => {
+        const _dataSpy = {
+            ...dataSpy,
+            error: true,
+        };
+        useApiRequestSpy.mockReturnValue(_dataSpy);
+        const { getByTestId } = renderedComponent();
+
+        expect(getByTestId('error')).toBeInTheDocument();
+    });
+
+    test('Should display loading if api response is loading', () => {
+        const _dataSpy = {
+            ...dataSpy,
+            isLoading: true,
+        };
+        useApiRequestSpy.mockReturnValue(_dataSpy);
+        const { getByTestId } = renderedComponent();
+
+        expect(getByTestId('loading')).toBeInTheDocument();
     });
 });
--- blog-react/src/pages/SearchPost/index.tsx
+++ blog-react/src/pages/SearchPost/index.tsx
@@ -4,6 +4,8 @@ import { useApiRequests } from '../../hooks/useApiRequest';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import { Post } from '../../Interfaces/Post';
 import Input from '../../Components/Form/Input';
+import { Card } from '../../Components/Posts/Card';
+import PostsList from '../../Components/Posts/PostsList/PostsList';
 
 type Options = {
     search?: string;
@@ -12,7 +14,7 @@ type Options = {
 
 function SearchPost() {
     const [options, setOptions] = useState<Options>({} as Options);
-    const { data, error, isLoading } = useApiRequests(`http://localhost/api/posts/search`, options);
+    const { data, error, isLoading, fetchData } = useApiRequests(`http://localhost/api/posts/search`, options, true);
 
     function handleChangeOptions(e: ChangeEvent<HTMLInputElement>) {
         setOptions({
@@ -20,16 +22,22 @@ function SearchPost() {
             [e.target.name]: e.target.value,
         });
     }
-    console.log('Api', options, error, isLoading);
+    console.log('Api', data, error, isLoading);
 
     return (
         <DefaultLayout>
             <Input name="search" onChange={(e) => handleChangeOptions(e)} icon={BsSearch} placeholder="Search" />
+            <button onClick={fetchData}>Serach</button>
             <div>
-                {data &&
-                    data.map((post: Post) => {
-                        return <li key={post.id}>{post.title}</li>;
-                    })}
+                <PostsList data={data.data} error={error} isLoading={isLoading} />
+                {/*     {data &&
+                    data.data.map((post: Post) => {
+                        return (
+                            <Card.root key={post.id}>
+                                <Card.content post={post} />
+                            </Card.root>
+                        );
+                    })} */}
             </div>
         </DefaultLayout>
     );
